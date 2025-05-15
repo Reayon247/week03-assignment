0b16fa2df66f76fe07c596c934ccd30868876f50
Ryan Nugent <ryan.nugent247@gmail.com>
2025-05-15 14:45:53 +0100

    feat: can collect data from the API and add it to the DOM

--- app.js
+++ app.js
@@ -8,12 +8,35 @@ let catPetPerSec = 0;
 
 //Shop
 
+const shopContainer = document.getElementById("store");
+
 async function getShopAPI() {
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
   );
   console.log("Response:", response);
-  const shopJson = await response.json();
+  var shopJson = await response.json();
   console.log("json data:", shopJson);
+  return shopJson;
 }
-getShopAPI();
+// getShopAPI();
+
+function makeTheShop(array) {
+  for (let i = 0; i < array.length; i++) {
+    newDiv = document.createElement("div");
+    newDiv.className = "shop-upgrade";
+
+    newH3 = document.createElement("h3");
+    newH3.textContent = `Upgrade: ${array[i].name}`;
+
+    shopContainer.appendChild(newDiv);
+    newDiv.appendChild(newH3);
+  }
+}
+
+async function putShopAndArray() {
+  const fetchedArray = await getShopAPI();
+  makeTheShop(fetchedArray);
+}
+
+putShopAndArray();
